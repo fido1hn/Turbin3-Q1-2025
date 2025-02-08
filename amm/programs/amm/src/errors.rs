fb18675c5cfda4f5c7e908e7a3b33a3579452fdf
fido1hn <confidoekeanya@gmail.com>
2025-02-08 06:32:14 +0100

    amm; public instructions

--- amm/programs/amm/src/errors.rs
+++ amm/programs/amm/src/errors.rs
@@ -2,5 +2,6 @@ use anchor_lang::error_code;
 
 #[error_code]
 pub enum AmmError {
+    #[msg("Some Error Occured!")]
     SomeError,
 }
--- amm/programs/amm/src/lib.rs
+++ amm/programs/amm/src/lib.rs
@@ -14,11 +14,18 @@ declare_id!("4UNeQ86dY6CRSMJvarSA4UTnjJQCxsUbDnnMVp3XAD7f");
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, claim_amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(claim_amount, max_x, max_y)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
